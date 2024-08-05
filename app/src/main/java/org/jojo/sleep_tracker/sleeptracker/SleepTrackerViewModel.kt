819497f807b2cbf9c1fd274d551dfc561c40a58a
Zhaoshan-Duan <joshua.duan47@gmail.com>
2024-08-04 17:50:41 -0700

    add viewModel job that: allows us to cancel all coroutines started by SleepTrackerViewModel when the viewModel is no longer used and destoryed so we don't end up with coroutines that have nowhere to return to

--- app/src/main/java/org/jojo/sleep_tracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/org/jojo/sleep_tracker/sleeptracker/SleepTrackerViewModel.kt
@@ -1,8 +1,8 @@
 package org.jojo.sleep_tracker.sleeptracker
 
 import android.app.Application
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.Job
 import org.jojo.sleep_tracker.database.SleepDatabaseDao
 
 /**
@@ -12,4 +12,7 @@ class SleepTrackerViewModel(
     // access to the database
     val database: SleepDatabaseDao,
     application: Application) : AndroidViewModel(application) {
+        private var viewModelJob = Job()
+
+
 }
